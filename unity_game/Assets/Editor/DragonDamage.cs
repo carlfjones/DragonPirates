c9fb6807e2f822705abab8d7e4c41158f6bc3fa3
Danny <mtme13@hotmail.com>
2019-08-08 16:35:11 +0100

    test: add first test case * fix compilation errors

---
+++ unity_game/Assets/Editor/DragonDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class DragonDamage
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void DragonDamageSimplePasses()
+        {
+			// Use the Assert class to test conditions
+			
+			//System.Timers.Timer(5000);
+			
+
+		}
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator DragonDamageWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
