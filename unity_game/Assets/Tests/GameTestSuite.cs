f209803b9455c867355dcca0b69bb5398005a4a7
Danny <mtme13@hotmail.com>
2019-08-09 14:04:57 +0100

    test: Add test structure for first unit test

---
+++ unity_game/Assets/Tests/GameTestSuite.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class GameTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void GameTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator GameTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
