930b71fba619e207f924b5b01a3c3e79b065641d
Danny <mtme13@hotmail.com>
2019-08-15 10:55:06 +0100

    WIP: getting playername from input field

---
+++ unity_game/Assets/Scripts/DetectText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // access the InputFieldComponent for the object linked to this script(i.e., InputField)
+        // use the event onEndEdit, which means that we will be tracking when the user has finished editing his/her text.
+        // When this happens, we will call a function called displayText 
+        Debug.Log($"input field: {gameObject.GetComponent<InputField>()}");
+        gameObject.GetComponent<InputField>().onEndEdit.AddListener(displayText);
+    }
+
+    private void displayText(string textInField)
+    {
+        print(textInField);
+    }
+}
---
+++ unity_game/Assets/Scripts/SaveName.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SaveName : MonoBehaviour
+{
+    // Find the input field and grab the text thats been typed in
+    public void submitName()
+    {
+        Debug.Log($"name {GameObject.FindWithTag("PlayerName").GetComponent<TMP_InputField()}>>");
+        string name = GameObject.FindWithTag("PlayerName").GetComponent<InputField>().text;
+        print("Saving " + name);
+        Debug.Log($"I am {name}");
+    }
+}
